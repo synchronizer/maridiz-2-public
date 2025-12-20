aaf55cbcbf581ad7d83dbd03d4d0159d20701bd4
synchronizer <synchronizer@yandex.ru>
2025-12-20 15:00:55 +0300

    M

--- script/header-change-color.js
+++ script/header-change-color.js
@@ -1,51 +1,24 @@
-const header = document.querySelector('.header');
 
-const header__bottomTriggerObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
+Array.from(document.querySelectorAll('.header')).forEach(header => {
+    const triggerObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
 
+            const colorScheme = entry.target.getAttribute('data-trigger-header-color-scheme').split('/');
 
-        if (entry.isIntersecting && entry.boundingClientRect.top <= 100) {
-            // console.log('Элемент полностью появился вверху страницы');
-            header.setAttribute('data-color-scheme', '');
-        }
-        if (!entry.isIntersecting && entry.boundingClientRect.top <= 0) {
-            // console.log('Элемент коснулся верхней границы');
-            header.setAttribute('data-color-scheme', entry.target.getAttribute('data-trigger-header-color-scheme'))
-        }
+            if (entry.boundingClientRect.top <= header.offsetHeight) {
+                    header.setAttribute('data-color-scheme', colorScheme[1])
+            } else {
+                header.setAttribute('data-color-scheme', colorScheme[0]);
+            }
 
-
+        });
+    }, {
+        root: null,
+        threshold: 0,
+        rootMargin: `-${header.offsetHeight}px 0px 0px 0px`,
     });
-}, {
-    root: null,
-    threshold: 1,
-});
 
-
-const header__topTriggerObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting && entry.boundingClientRect.top <= 0) {
-            // console.log('Элемент начал появляться вверху страницы');
-            header.setAttribute('data-color-scheme', entry.target.getAttribute('data-trigger-header-color-scheme'))
-
-        }
-        if (!entry.isIntersecting && entry.boundingClientRect.top <= 0) {
-            console.log('Элемент ушёл наверх');
-            header.setAttribute('data-color-scheme', '');
-        }
-
-    });
-}, {
-    root: null,
-    threshold: 0,
-});
-
-
-
-
-
-
-
-Array.from(document.querySelectorAll('[data-trigger-header-color-scheme]')).forEach(item => {
-    header__topTriggerObserver.observe(item);
-    header__bottomTriggerObserver.observe(item);
+    Array.from(document.querySelectorAll('[data-trigger-header-color-scheme]')).forEach(item => {
+        triggerObserver.observe(item);
+    })
 })
